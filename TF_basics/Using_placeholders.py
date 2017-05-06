39f28af1e5088341c3762ffa264bc1dd8cb8c32c
akash9182 <akash9182akash@gmail.com>
2017-05-06 19:48:52 +0530

    changing directory

---
+++ TF_basics/Using_placeholders.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import tensorflow as tf
+
+a = tf.placeholder(tf.int16)
+b = tf.placeholder(tf.int16)
+
+add = tf.add(a,b)
+mul = tf.multiply(a,b)
+
+# # Same op?
+print(add)
+print(a + b)
+print(mul)
+print(a * b)
+
+with tf.Session() as sess:
+	print(sess.run(add, feed_dict = {a:2, b:5}))
+
+	feed = {a:1, b:5}
+	print(sess.run(mul, feed_dict = feed))
